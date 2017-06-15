7e30df7c261c8397cac5d5bb1de90eaf19b53658
jevonthomas <jevon.g.thomas@gmail.com>
2017-06-15 13:18:15 -0500

    added japanese.js to the index and adjusted the size of the text areas. Also got portugues.js complete

--- portuguese.js
+++ portuguese.js
@@ -1,11 +1,9 @@
 var Localization = (function (localObj){
-		var portugueseWords = ["Thanksgiving", "Christmas"];
+		var portugueseWords = "Feliz Ano Novo! Você será meu namorado e vai beber comigo no dia de São Patrício até que nós passemos e nos levante novamente na Páscoa e obtenhamos uma vantagem para a compra de fogos de artifício para o Dia da Independência, a fim de celebrar a nossa liberdade de nos vestir no Halloween, coma a Turquia No Dia de Ação de Graças e comemorar os feriados.";
 		toPortuguese = function() {
-			console.log("portuguese");
 			let languageFlag = document.getElementById("optionSelect").value;//value of option
-			console.log("langFlag", languageFlag);
 			if (languageFlag === "portuguese"){
-				document.getElementById("translatedTxt").innerHTML = portugueseWords.join(" ");
+				document.getElementById("translatedTxt").innerHTML = portugueseWords;
 				return localObj.portuguese;
 			}
 		}
