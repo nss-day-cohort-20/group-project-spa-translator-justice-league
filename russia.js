450ddd1b203e75f1b65e169e73025b0281408b40
Sam Staff <samstaff@Sams-MacBook-Pro.local>
2017-06-15 13:19:09 -0500

    Russian Lexicons

--- russia.js
+++ russia.js
@@ -1,11 +1,11 @@
 var Localization = (function (localObj){
-		var russianWords = ["New Years", "Christmas"];
+		var russianWords = "С новым годом! Будете ли вы моим Валентином и будете пить со мной в День Святого Патрика, пока мы не выйдем и не воскреснем на Пасху и не начнем покупать фейерверки в День независимости, чтобы отпраздновать нашу свободу одеваться на Хэллоуин, есть индейку На День Благодарения и отпраздновать Праздники.";
 		toRussian = function() {
-			console.log("russian");
+			// console.log("russian");
 			let languageFlag = document.getElementById("optionSelect").value;//value of option
-			console.log("langFlag", languageFlag);
+			// console.log("langFlag", languageFlag);
 			if (languageFlag === "russian"){
-				document.getElementById("translatedTxt").innerHTML = russianWords.join(" ");
+				document.getElementById("translatedTxt").innerHTML = russianWords;
 				return localObj.russian;
 			}
 		}
